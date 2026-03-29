b94b758a7bd34f952e2d19f46d2dbad1111d7b93
Sydnee <ravensramen@gmail.com>
2026-03-29 10:09:30 -0700

    Part 3.2 complete

--- main.py
+++ main.py
@@ -65,13 +65,28 @@ for i in range(32):
     if i in primeSet:
         PrimeBDD = PrimeBDD | int_to_bdd(i, xvars)
 
-#test cases
-print("RR(27,3):", check_edge(R, 27, 3))    # should be True
+#test cases for 3.1
+""" print("RR(27,3):", check_edge(R, 27, 3))    # should be True
 print("RR(16,20):", check_edge(R, 16, 20))  # should be False
 print("EVEN(14):", check_node(EvenBDD, 14)) # should be True
 print("EVEN(13):", check_node(EvenBDD, 13)) # should be False
 print("PRIME(7):", check_node(PrimeBDD, 7)) # should be True
-print("PRIME(2):", check_node(PrimeBDD, 2)) # should be False
+print("PRIME(2):", check_node(PrimeBDD, 2)) # should be False """
+
+#3.2 Compute BDD RR2, node pairs that can reach eachother in up to 2 steps
+zvars = [bddvar('z', i) for i in range(5)] #need intermediate z variable for x->z z->y
+
+#use the R BDD determined from part 3.1
+RXY = R.compose({yvars[i]: zvars[i] for i in range(5)}) #replace Y connections with Z
+RYZ = R.compose({xvars[i]:zvars[i] for i in range(5)}) #replace X connections with Z
+
+combinedBDD = RXY & RYZ 
+#create final BDD with x->y connections, including those with intermediary z edge
+RR2 = combinedBDD.smoothing(frozenset(zvars[i] for i in range(5))) 
+
+#test cases for 3.2
+print("RR2(27, 6):", check_edge(RR2, 27, 6)) #should be true
+print("RR2(27, 9):", check_edge(RR2, 27,9)) #should be false
 
 
 #use exit() to end terminal
